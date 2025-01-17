45b3d71d7c11c247458bf0f9e077bed50bb2af1a
Ahmed Negm <160288929+x7007x@users.noreply.github.com>
2025-01-17 18:06:32 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r", encoding="utf-8") as fh:
 
 setup(
     name="pinterest_downloader",
-    version="0.1.0",
+    version="1.0.0",
     author="Ahmed Negm",
     author_email="a7mednegm.x@gmail.com",
     description="A Pinterest media downloader",
