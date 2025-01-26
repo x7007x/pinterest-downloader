b597829bbfc8cadfb42ee65eca81991d9cef97d4
Ahmed Negm <160288929+x7007x@users.noreply.github.com>
2025-01-26 17:59:21 +0200

    Update __init__.py

--- pinterest_downloader/__init__.py
+++ pinterest_downloader/__init__.py
@@ -1,4 +1,4 @@
-from .downloader import download_pinterest_media, pinterest_search, search_and_download_pinterest
+from .downloader import download_pinterest_media, pinterest_search
 
-__all__ = ['download_pinterest_media', 'pinterest_search', 'search_and_download_pinterest']
+__all__ = ['download_pinterest_media', 'pinterest_search']
 
